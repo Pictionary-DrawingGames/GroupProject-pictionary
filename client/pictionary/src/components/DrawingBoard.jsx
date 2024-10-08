5ebc0ffd2558a325a9e9d0327908dc69bccf87cf
dhiaulhaq <dmnaufal7@gmail.com>
2024-10-08 17:19:30 +0700

    CLIENT: DrawingBoard: Handle drawing on board

--- client/pictionary/src/components/DrawingBoard.jsx
+++ client/pictionary/src/components/DrawingBoard.jsx
@@ -1,4 +1,83 @@
-export default function DrawingBoard() {
+import React, { useEffect, useRef } from "react";
+
+export default function DrawingBoard({ sendData, word }) {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const isDrawing = useRef(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.strokeStyle = "black"; // ubah warna
+    context.lineWidth = 2; // ketebalan garis
+    contextRef.current = context;
+  }, []);
+
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const coords = getMouseCoords(canvasRef.current, e);
+    const context = contextRef.current;
+    context.beginPath();
+    context.moveTo(coords.x, coords.y);
+
+    const data = {
+      action: "start",
+      currentX: coords.x,
+      currentY: coords.y,
+    };
+
+    sendData(JSON.stringify(data));
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDrawing.current) return;
+    const coords = getMouseCoords(canvasRef.current, e);
+    const context = contextRef.current;
+    context.lineTo(coords.x, coords.y);
+    context.stroke();
+
+    const data = {
+      action: "draw",
+      currentX: coords.x,
+      currentY: coords.y,
+    };
+
+    sendData(JSON.stringify(data));
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+    const context = contextRef.current;
+    context.closePath();
+
+    const data = {
+      action: "stop",
+    };
+
+    sendData(JSON.stringify(data));
+  };
+
+  const getMouseCoords = (canvas, e) => {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    };
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center relative pt-4">
@@ -10,7 +89,7 @@ export default function DrawingBoard() {
           id="gameCanvas"
           width={400}
           height={400}
-          //   ref={canvasRef}
+          ref={canvasRef}
           className="bg-white rounded-lg border-2 border-[#431407]"
         ></canvas>
       </div>
